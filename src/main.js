5204bb201ced85ef4c6af3cf5be67f517933a4a9
tsy <tsyhust@126.com>
2021-04-20 16:43:27 +0800

    master

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/icon/iconfont.css'
+//引入summernote
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'popper.js'
+import 'summernote'
+import 'summernote/lang/summernote-zh-CN.js'
+import 'summernote/dist/summernote.css'
 
-Vue.config.productionTip = false
+import './assets/css/mystyle.css'
+
+Vue.use(ElementUI)
+Vue.config.devtools = true;
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
